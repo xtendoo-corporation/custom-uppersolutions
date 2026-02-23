3e5a44a68cede36f1842bb53febcf047ea9203ad
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2026-02-23 10:52:42 +0100

    logo removed

---
+++ uppersolutions_portal_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 UpperSolutions
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Portal Customization",
+    "version": "19.0.1.0.0",
+    "summary": "Personalizaciones para el portal de clientes (ocultar logo, etc).",
+    "author": "UpperSolutions",
+    "license": "AGPL-3",
+    "category": "Portal",
+    "depends": [
+        "portal",
+    ],
+    "data": [
+        "views/portal_templates.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
