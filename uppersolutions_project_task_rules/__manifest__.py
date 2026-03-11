23db900a3ff262bc9bcebfeb669672e36b062c24
Gokac9 <theabraham9@gmail.com>
2026-03-11 08:20:48 +0100

    add module uppersolutions_project_task_rules

---
+++ uppersolutions_project_task_rules/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'UpperSolutions Project Task Rules',
+    'version': '19.0.1.0.0',
+    'category': 'Project',
+    'summary': 'Restricts project task visibility for non-admins to their assigned tasks only',
+    'author': 'UpperSolutions',
+    'website': 'https://www.uppersolutions.com',
+    'depends': ['project'],
+    'data': [
+        'security/rules.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
