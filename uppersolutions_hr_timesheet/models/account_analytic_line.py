5a91e7a70ba98a8827cf5e45b383fb23b5313002
Gokac9 <theabraham9@gmail.com>
2026-04-07 14:41:39 +0200

    actualizar fecha en partes de horas

--- uppersolutions_hr_timesheet/models/account_analytic_line.py
+++ uppersolutions_hr_timesheet/models/account_analytic_line.py
@@ -9,11 +9,26 @@ class AccountAnalyticLine(models.Model):
         compute='_compute_display_helpdesk_ticket_id',
         readonly=True,
     )
+    display_ticket_date = fields.Date(
+        string='Fecha',
+        compute='_compute_display_ticket_date',
+        readonly=True,
+    )
 
     @api.depends('helpdesk_ticket_id', 'task_id', 'task_id.helpdesk_ticket_id')
     def _compute_display_helpdesk_ticket_id(self):
         for line in self:
             line.display_helpdesk_ticket_id = line.helpdesk_ticket_id or line.task_id.helpdesk_ticket_id
+
+    @api.depends(
+        'date',
+        'helpdesk_ticket_id.estimated_date',
+        'task_id.helpdesk_ticket_id.estimated_date',
+    )
+    def _compute_display_ticket_date(self):
+        for line in self:
+            ticket = line.helpdesk_ticket_id or line.task_id.helpdesk_ticket_id
+            line.display_ticket_date = ticket.estimated_date or line.date
 
     def action_open_task(self):
         self.ensure_one()
